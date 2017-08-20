f209feffc630711054649663f2b6c9c9f89ac6d4
abdulsamad aliyu <abdulsamadaliyu17@gmail.com>
2017-08-20 17:26:16 +0100

    I added myself to the participant's list and changed the logo color

--- participants.php
+++ participants.php
@@ -1,7 +1,7 @@
 <?php require_once('header.php'); ?>
 
 <div id="banner-content" class="row clearfix">
-  <h1 class="team3_heading"> Viewing all Participants</h1>
+  <h1 class="team3_heading">Viewing all Participants...</h1>
   <table class="table user-list" width="80%">
 					<thead>
 						<tr>
@@ -24,6 +24,23 @@
 								<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
 								<a href="#" class="user-link">Emmanuel Joseph</a>
 								<span class="user-subhead">Email: jekayode@live.com</span>
+							</td>
+							<td>
+								Team 3
+							</td>
+						</tr>
+            <!--
+              Each Participant Ends
+            -->
+            <!--
+              each Participant Starts
+            -->
+						<tr>
+              <td class="team3_center_td"> 2.</td>
+							<td>
+								<img id= "abdul-photo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDw44BhHjka-ySW9YsGhiD40sWcISHEihLl0X40-7NLT83honDEA" alt="">
+								<a href="https://instagram.com/glidez007" class="user-link">Abdulsamad Aliyu</a>
+								<span class="user-subhead">Email: abdulsamadaliyu17@gmail.com</span>
 							</td>
 							<td>
 								Team 3
